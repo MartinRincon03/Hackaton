f9daee12f279c21d48b883c594101d66ec4caeab
FranEscobarG <escobar.gutierrez.3@gmail.com>
2023-06-02 13:35:41 -0600

    [FIX] View AboutUs finished

--- src/pages/Nosotros.jsx
+++ src/pages/Nosotros.jsx
@@ -1,5 +1,8 @@
-import "../assets/styles/Nosotros.css";
 import Title from "../components/atoms/Title";
+import ImgVision from '../assets/img/imgVision.webp' 
+import ImgMision from '../assets/img/imgMision.webp'
+import ImgObjetivos from '../assets/img/imgObjetivos.webp'
+import "../assets/styles/Nosotros.css";
 
 function Nosotros() {
   return (
@@ -18,33 +21,26 @@ function Nosotros() {
       <div className="row featurette custom-row">
         <div className="col-md-7">
           <h2 className="featurette-heading fw-normal lh-1">
-            <span className="text-body-secondary">Objetivo</span>
+            <span className="text-body-secondary caption-nosotros">Objetivos</span>
           </h2>
           <p className="lead">
-            <li>
-                <ul>La importancia de contar con una solución de vanguardia y al alcance de cualquier persona,  que permita monitorear el recorrido del caudal de ríos y arroyos.</ul>
-                <ul>Mecanismo de denuncia que permita hacer valer los derechos humanos de la salud y del acceso al agua</ul>
-            </li>
+            <ul>
+              <li className="item-objetivo">
+                Contar con una solución de vanguardia y al alcance de cualquier
+                persona, que permita monitorear situaciones que afecten a nuesto
+                estado y planeta
+              </li>
+              <li className="item-objetivo">
+                Ser un mecanismo de denuncia que permita hacer valer los
+                derechos humanos de la salud y del acceso al agua
+              </li>
+              <li className="item-objetivo">
+                Mejorar la calidad de vida de los ciudadanos de Chiapas
+              </li>
+            </ul>
           </p>
         </div>
-        <div className="col-md-5">
-          <svg
-            className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"
-            width="500"
-            height="500"
-            xmlns="http://www.w3.org/2000/svg"
-            role="img"
-            aria-label="Placeholder: 500x500"
-            preserveAspectRatio="xMidYMid slice"
-            focusable="false"
-          >
-            <title>Placeholder</title>
-            <rect width="100%" height="100%" fill="var(--bs-secondary-bg)" />
-            <text x="50%" y="50%" fill="var(--bs-secondary-color)" dy=".3em">
-              500x500
-            </text>
-          </svg>
-        </div>
+        <img className="imgNosotros" src={ImgObjetivos} alt="" />
       </div>
 
       <hr className="featurette-divider" />
@@ -52,32 +48,20 @@ function Nosotros() {
       <div className="row featurette custom-row">
         <div className="col-md-7 order-md-2">
           <h2 className="featurette-heading fw-normal lh-1">
-            <span className="text-body-secondary">Misión</span>
+            <span className="text-body-secondary caption-nosotros">Misión</span>
           </h2>
           <p className="lead">
-            Another featurette? Of course. More placeholder content here to give
-            you an idea of how this layout would work with some actual
-            real-world content in place.
+            Nuestra misión es contribuir al cuidado y preservación del medio
+            ambiente en el estado de Chiapas, mediante el desarrollo de un
+            software innovador que promueva la conciencia ambiental, facilite la
+            identificación de problemáticas ambientales y fomente la
+            participación ciudadana en la búsqueda de soluciones. Buscamos
+            generar un impacto positivo en la conservación de los recursos
+            naturales, la reducción de la deforestación y el fomento de
+            prácticas sostenibles en el estado.
           </p>
         </div>
-        <div className="col-md-5 order-md-1">
-          <svg
-            className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"
-            width="500"
-            height="500"
-            xmlns="http://www.w3.org/2000/svg"
-            role="img"
-            aria-label="Placeholder: 500x500"
-            preserveAspectRatio="xMidYMid slice"
-            focusable="false"
-          >
-            <title>Placeholder</title>
-            <rect width="100%" height="100%" fill="var(--bs-secondary-bg)" />
-            <text x="50%" y="50%" fill="var(--bs-secondary-color)" dy=".3em">
-              500x500
-            </text>
-          </svg>
-        </div>
+        <img className="imgNosotros" src={ImgVision} alt="" />
       </div>
 
       <hr className="featurette-divider" />
@@ -85,33 +69,22 @@ function Nosotros() {
       <div className="row featurette custom-row">
         <div className="col-md-7">
           <h2 className="featurette-heading fw-normal lh-1">
-            <span className="text-body-secondary">Visión</span>
+            <span className="text-body-secondary caption-nosotros">Visión</span>
           </h2>
           <p className="lead">
-            And yes, this is the last block of representative placeholder
-            content. Again, not really intended to be actually read, simply here
-            to give you a better view of what this would look like with some
-            actual content. Your content.
+            Nuestra visión es convertirnos en una herramienta de referencia para
+            el cuidado del medio ambiente en Chiapas, siendo reconocidos por
+            nuestra contribución significativa en la concienciación y acción
+            ambiental. Aspiramos a ser una plataforma que integre información
+            confiable, recursos educativos y herramientas interactivas para
+            empoderar a los ciudadanos, instituciones gubernamentales y
+            organizaciones ambientales en la protección y restauración del
+            entorno natural en Chiapas. Buscamos generar un cambio positivo y
+            duradero en la relación entre las personas y el medio ambiente,
+            promoviendo la sostenibilidad y la calidad de vida en la región.
           </p>
         </div>
-        <div className="col-md-5">
-          <svg
-            className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"
-            width="500"
-            height="500"
-            xmlns="http://www.w3.org/2000/svg"
-            role="img"
-            aria-label="Placeholder: 500x500"
-            preserveAspectRatio="xMidYMid slice"
-            focusable="false"
-          >
-            <title>Placeholder</title>
-            <rect width="100%" height="100%" fill="var(--bs-secondary-bg)" />
-            <text x="50%" y="50%" fill="var(--bs-secondary-color)" dy=".3em">
-              500x500
-            </text>
-          </svg>
-        </div>
+        <img className="imgNosotros" src={ImgMision} alt="" />
       </div>
 
       <hr className="featurette-divider" />
