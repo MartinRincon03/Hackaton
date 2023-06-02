435f1fae05a142c37e7f78ef82e01f0f6ccf27d0
FranEscobarG <escobar.gutierrez.3@gmail.com>
2023-06-02 10:41:39 -0600

    [ADD] Creacion de las vistas Saber mas y NosotrosK

--- src/pages/Nosotros.jsx
+++ src/pages/Nosotros.jsx
@@ -1,9 +1,122 @@
+import "../assets/styles/Nosotros.css";
+import Title from "../components/atoms/Title";
+
 function Nosotros() {
-    return ( 
-        <div>
-            <h1>HELLO SOY INFORMACION SOBRE NOSOTROS</h1>
+  return (
+    <div className="sobre-nosotros">
+      <div className="cover-3">
+        <Title msn={"Conoce sobre nosotros"} />
+        {/* <p className="lead text-white ">
+          <b>
+            
+          </b>
+        </p> */}
+      </div>
+
+      <hr className="featurette-divider" />
+
+      <div className="row featurette custom-row">
+        <div className="col-md-7">
+          <h2 className="featurette-heading fw-normal lh-1">
+            <span className="text-body-secondary">Objetivo</span>
+          </h2>
+          <p className="lead">
+            <li>
+                <ul>La importancia de contar con una solución de vanguardia y al alcance de cualquier persona,  que permita monitorear el recorrido del caudal de ríos y arroyos.</ul>
+                <ul>Mecanismo de denuncia que permita hacer valer los derechos humanos de la salud y del acceso al agua</ul>
+            </li>
+          </p>
         </div>
-     );
+        <div className="col-md-5">
+          <svg
+            className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"
+            width="500"
+            height="500"
+            xmlns="http://www.w3.org/2000/svg"
+            role="img"
+            aria-label="Placeholder: 500x500"
+            preserveAspectRatio="xMidYMid slice"
+            focusable="false"
+          >
+            <title>Placeholder</title>
+            <rect width="100%" height="100%" fill="var(--bs-secondary-bg)" />
+            <text x="50%" y="50%" fill="var(--bs-secondary-color)" dy=".3em">
+              500x500
+            </text>
+          </svg>
+        </div>
+      </div>
+
+      <hr className="featurette-divider" />
+
+      <div className="row featurette custom-row">
+        <div className="col-md-7 order-md-2">
+          <h2 className="featurette-heading fw-normal lh-1">
+            <span className="text-body-secondary">Misión</span>
+          </h2>
+          <p className="lead">
+            Another featurette? Of course. More placeholder content here to give
+            you an idea of how this layout would work with some actual
+            real-world content in place.
+          </p>
+        </div>
+        <div className="col-md-5 order-md-1">
+          <svg
+            className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"
+            width="500"
+            height="500"
+            xmlns="http://www.w3.org/2000/svg"
+            role="img"
+            aria-label="Placeholder: 500x500"
+            preserveAspectRatio="xMidYMid slice"
+            focusable="false"
+          >
+            <title>Placeholder</title>
+            <rect width="100%" height="100%" fill="var(--bs-secondary-bg)" />
+            <text x="50%" y="50%" fill="var(--bs-secondary-color)" dy=".3em">
+              500x500
+            </text>
+          </svg>
+        </div>
+      </div>
+
+      <hr className="featurette-divider" />
+
+      <div className="row featurette custom-row">
+        <div className="col-md-7">
+          <h2 className="featurette-heading fw-normal lh-1">
+            <span className="text-body-secondary">Visión</span>
+          </h2>
+          <p className="lead">
+            And yes, this is the last block of representative placeholder
+            content. Again, not really intended to be actually read, simply here
+            to give you a better view of what this would look like with some
+            actual content. Your content.
+          </p>
+        </div>
+        <div className="col-md-5">
+          <svg
+            className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"
+            width="500"
+            height="500"
+            xmlns="http://www.w3.org/2000/svg"
+            role="img"
+            aria-label="Placeholder: 500x500"
+            preserveAspectRatio="xMidYMid slice"
+            focusable="false"
+          >
+            <title>Placeholder</title>
+            <rect width="100%" height="100%" fill="var(--bs-secondary-bg)" />
+            <text x="50%" y="50%" fill="var(--bs-secondary-color)" dy=".3em">
+              500x500
+            </text>
+          </svg>
+        </div>
+      </div>
+
+      <hr className="featurette-divider" />
+    </div>
+  );
 }
 
 export default Nosotros;
--- src/pages/SaberMas.jsx
+++ src/pages/SaberMas.jsx
@@ -1,14 +1,161 @@
+import "../assets/styles/SaberMas.css";
+import Title from "../components/atoms/Title";
+
 function SaberMas() {
-    return ( 
-        <div>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati nostrum deleniti, dolore iusto autem earum dolores. Suscipit vitae eos consequuntur voluptatibus? Debitis molestiae quasi at omnis enim amet. Impedit, qui.
-            Iste inventore sint officia vero ducimus molestiae quam. Mollitia quae debitis explicabo quia impedit distinctio totam quidem possimus odit rem adipisci perspiciatis pariatur cum eligendi necessitatibus veritatis sit, voluptatum tenetur.
-            Molestias eum libero delectus obcaecati repudiandae, rerum eos non soluta vel recusandae fugiat labore. Sint facilis vel nulla rerum quidem quia, a, et odit porro, quasi sequi labore molestias voluptate?
-            Nobis perspiciatis odit veritatis consequatur nesciunt perferendis, eum aut autem neque, architecto consectetur illum deserunt obcaecati exercitationem asperiores? Rerum quibusdam, praesentium sed vel obcaecati perspiciatis hic doloremque delectus sit facere!
-            Non soluta excepturi voluptates illo eius facere. Officiis fuga reprehenderit modi molestiae repellendus reiciendis, possimus quod tempora iste, recusandae consequatur excepturi ad aliquam asperiores dolor molestias? Accusamus ipsum numquam neque.</p>
-            <h1>HELLO SOY SABER MÁS</h1>
+  return (
+    <div>
+      <div className="cover-second">
+        <Title msn={"¡Conoce más sobre cómo puedes cuidar tu localidad!"} />
+        <p className="lead text-white ">
+          <b>Al cuidar tu localidad, construyes un entorno más seguro, limpio y
+          vibrante para ti y para los demás.</b>
+        </p>
+      </div>
+      <div className="hero text-dark text-center py-5">
+        <h3 className="display-4 text-dark">
+            ¡Descubre cómo puedes cuidar tu localidad y proteger tu entorno!
+        </h3>
+        <p className="lead">
+            ¡Cuida tu localidad y protege el medio ambiente! Ahorra agua, combate la deforestación y reflexiona sobre tu responsabilidad individual. Cada acción cuenta: separa la basura, reduce el plástico y promueve la educación ambiental. ¡Únete y haz la diferencia hoy mismo!
+        </p>
+      </div>
+
+      <div className="container mt-5">
+        <h2 className="subtitle">Sobre el agua</h2>
+        <div className="row row-cols-1 row-cols-md-2 g-4">
+          <div className="col">
+            <div className="card">
+              <img
+                src="https://revista.une.org/images/revistas/articulos/41551a6e4d77e372ad6241d528794650.jpg"
+                className="card-img-top"
+                alt="..."
+              />
+              <div className="card-body">
+                <h5 className="card-title">
+                  La disponibilidad de agua, su gestión sostenible y el
+                  saneamiento para todos.
+                </h5>
+                <a
+                  href="https://www.agenda2030.chiapas.gob.mx/objetivo-6-agua-limpia-y-saneamiento/"
+                  className="styled-link"
+                >
+                  Ver más
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col">
+            <div className="card">
+              <img
+                src="https://images.unsplash.com/photo-1530587191325-3db32d826c18?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1074&q=80"
+                className="card-img-top"
+                alt="..."
+              />
+              <div className="card-body">
+                <h5 className="card-title">
+                  Contaminación de ríos y arroyos es cada vez más preocupante.
+                </h5>
+                <a
+                  href="https://agua.org.mx/chiapas-contaminacion-de-rios-y-arroyos-es-cada-vez-mas-preocupante-ultimatum/"
+                  className="styled-link"
+                >
+                  Ver más
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
-     );
+        <br></br><br></br>
+        <h2 className="subtitle mt-5">Deforestación</h2>
+        <div className="row row-cols-1 row-cols-md-2 g-4">
+          <div className="col">
+            <div className="card">
+              <img
+                src="https://rde.inegi.org.mx/wp-content/uploads/2021/04/RDE33_art02_img03.png"
+                className="card-img-top"
+                alt="..."
+              />
+              <div className="card-body">
+                <h5 className="card-title">
+                  Comparación de métodos de detección de cambio forestal.
+                </h5>
+                <a
+                  href="https://rde.inegi.org.mx/wp-content/uploads/2021/04/RDE33_02.pdf"
+                  className="styled-link"
+                >
+                  Ver más
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col">
+            <div className="card">
+              <img
+                src="https://www.scielo.org.mx/img/revistas/mb/v24nspe//2448-7597-mb-24-spe-e2401897-gf2.png"
+                className="card-img-top"
+                alt="..."
+              />
+              <div className="card-body">
+                <h5 className="card-title">
+                  Diagnóstico sobre determinantes de deforestación en Chiapas.
+                </h5>
+                <a
+                  href="http://sis.cnf.gob.mx/wp-content/plugins/conafor-files/2018/nacional/catalogo/biblioteca/101.pdf"
+                  className="styled-link"
+                >
+                  Ver más
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <br></br><br></br>
+        <h2 className="subtitle mt-4">Deterioro ambiental</h2>
+        <div className="row row-cols-1 row-cols-md-2 g-4">
+          <div className="col">
+            <div className="card">
+              <img
+                src="https://i0.wp.com/agronoticias.com.mx/wp-content/uploads/2017/06/informaparaguaypuntocom.jpg?resize=600%2C280&ssl=1"
+                className="card-img-top"
+                alt="..."
+              />
+              <div className="card-body">
+                <h5 className="card-title">
+                  ESTADÍSTICAS A PROPÓSITO DEL DÍA MUNDIAL DEL MEDIO AMBIENTE.
+                </h5>
+                <a
+                  href="https://www.inegi.org.mx/contenidos/saladeprensa/aproposito/2020/ambiente2020_Nal.pdf"
+                  className="styled-link"
+                >
+                  Ver más
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col">
+            <div className="card">
+              <img
+                src="https://www.nueva-iso-14001.com/wp-content/uploads/2020/12/medio-ambiente.jpg"
+                className="card-img-top"
+                alt="..."
+              />
+              <div className="card-body">
+                <h5 className="card-title">Población y medio ambiente.</h5>
+                <a
+                  href="https://apps1.semarnat.gob.mx:8443/dgeia/informe18/tema/cap1.html"
+                  className="styled-link"
+                >
+                  Ver más
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <br></br>
+    </div>
+  );
 }
 
 export default SaberMas;
